f296eed3836f680f1caad4ef95c5d7790de6b6bc
Комаров Дмитрий Витальевич <105514231+Myrmlok@users.noreply.github.com>
2025-04-18 00:42:58 +0300

    Delete app directory

--- app/src/main/java/com/neurotech/callibrineurosdkdemo/screens/info/InfoViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.neurotech.callibrineurosdkdemo.screens.info
-
-import androidx.databinding.ObservableField
-import androidx.lifecycle.ViewModel
-import com.neurotech.callibrineurosdkdemo.callibri.CallibriController
-
-class InfoViewModel : ViewModel() {
-    var infoText = ObservableField(CallibriController.fullInfo())
-}
--- app/src/main/java/com/neurotech/callibrineurosdkdemo/screens/search/DeviceListItem.kt
+++
@@ -1,5 +0,0 @@
-package com.neurotech.callibrineurosdkdemo.screens.search
-
-import com.neurosdk2.neuro.types.SensorInfo
-
-data class DeviceListItem (val name: String, val address:String, var inProgress: Boolean, val sInfo: SensorInfo)
